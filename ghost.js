84bace12f6beb3be5f062179146bbb774471f90e
Travis <bilge+travis@scriptfusion.com>
2014-10-18 20:14:05 +0000

    Built product from 722a32c989d4b617721812b3aa1b3162bd2e2891.

--- ghost.js
+++ ghost.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-require('fs').symlinkSync('/usr/lib/node_modules/ghost/content', '/var/ghost');
-
 require('ghost')({config: '/etc/ghost/config.js'}).then(function(ghost) {
     process.env.NANO_URL && (ghost.config.url = process.env.NANO_URL);
 
